a7e081568b13aab3c4f0175b7ece6717a43cd39a
James Colley <jmcolley@proton.me>
2024-04-13 19:49:54 +0100

    Revert "Turned into a plugin; innumerable other changes"
    This reverts commit 341a768a71cadfe8aece872818c439ab70cb89d7.

--- Plugins/UnEViN/Content/Python/validateScriptsPath.py
+++
@@ -1,6 +0,0 @@
-import sys
-from pathlib import Path
-
-unevinScriptsFolderPath = Path(sys.argv[1])
-if not unevinScriptsFolderPath.is_dir() and not unevinScriptsFolderPath.is_file():
-    unevinScriptsFolderPath.mkdir()
